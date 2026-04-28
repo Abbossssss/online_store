7b948fed0c2c4257a0830679a52b1031986d4d90
Abbos Urazov <urazovabbosjon@gmail.com>
2026-04-28 19:47:20 +0500

    Update views.py

--- app_shop/views.py
+++ app_shop/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+from .models import Product  
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'app_shop/register.html', {'form': form})
+
+
+@login_required
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'app_shop/product_list.html', {'products': products})
+
+@login_required
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'app_shop/product_detail.html', {'product': product})
