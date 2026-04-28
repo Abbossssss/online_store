e3809094d79b9a081683d0ea4ad183593d42d8e3
Abbos Urazov <urazovabbosjon@gmail.com>
2026-04-28 17:23:20 +0500

    Add app_shop app' Register app_shop in sittings.pyfile

---
+++ app_shop/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Kategoriya nomi", max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(verbose_name="Kategoriya nomi", max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
